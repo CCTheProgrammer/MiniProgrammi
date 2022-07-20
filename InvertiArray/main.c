6772f651e87d980e271f039621c2b9586937335b
CCTheProgrammer <95089630+CCTheProgrammer@users.noreply.github.com>
2022-07-20 14:40:54 +0200

    Delete InvertiArray directory

--- InvertiArray/main.c
+++
@@ -1,8 +0,0 @@
-int CercaNumero(int v[], int len, int n);
-
-int main() {
-	int v[] = {12, 5, 4, 56, 10}; //inizializzazione del vettore
-
-	CercaNumero(v, 5, 10); //chiamata funzione CercaNumero
-	CercaNumero(v, 5, 67); //chiamata funzione CercaNumero
-}
